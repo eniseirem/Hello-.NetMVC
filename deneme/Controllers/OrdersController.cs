0feab8d854517078abed0278c76492cda6c2c004
EniseIrem Colak <EniseIrem.Colak@logo.com.tr>
2019-06-24 15:34:29 +0300

    Ajax Version for Edit/Create

---
+++ deneme/Controllers/OrdersController.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using deneme.Models;
+
+namespace deneme.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        // GET: Orders
+        public ActionResult Index()
+        {
+            var orders = db.Orders.Include(o => o.Customer).Include(o => o.Employee).Include(o => o.Shipper);
+            return View(orders.ToList());
+        }
+
+        // GET: Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: Orders/Create
+        public ActionResult Create()
+        {
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CompanyName");
+            ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "LastName");
+            ViewBag.ShipVia = new SelectList(db.Shippers, "ShipperID", "CompanyName");
+            return View();
+        }
+
+        // POST: Orders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+
+        /*   public ActionResult Create([Bind(Include = "OrderID,CustomerID,EmployeeID,OrderDate,RequiredDate,ShippedDate,ShipVia,Freight,ShipName,ShipAddress,ShipCity,ShipRegion,ShipPostalCode,ShipCountry")] Order order)
+           {
+               if (ModelState.IsValid)
+               {
+                   db.Orders.Add(order);
+                   db.SaveChanges();
+                   return RedirectToAction("Index");
+               }
+
+               ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CompanyName", order.CustomerID);
+               ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "LastName", order.EmployeeID);
+               ViewBag.ShipVia = new SelectList(db.Shippers, "ShipperID", "CompanyName", order.ShipVia);
+               return View(order);
+           }*/
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Order data)
+        {
+            
+            Order ordernew = new Order();
+            ordernew.ShipCountry = data.ShipCountry;
+            ordernew.ShipCity = data.ShipCity;
+            ordernew.ShipName = data.ShipName;
+            db.Orders.Add(ordernew);
+            db.SaveChanges();
+         return RedirectToAction("Index");
+        }
+
+        // GET: Orders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CompanyName", order.CustomerID);
+            ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "LastName", order.EmployeeID);
+            ViewBag.ShipVia = new SelectList(db.Shippers, "ShipperID", "CompanyName", order.ShipVia);
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+      
+        /* public ActionResult Edit([Bind(Include = "OrderID,CustomerID,EmployeeID,OrderDate,RequiredDate,ShippedDate,ShipVia,Freight,ShipName,ShipAddress,ShipCity,ShipRegion,ShipPostalCode,ShipCountry")] Order order)
+         {
+             if (ModelState.IsValid)
+             {
+                 db.Entry(order).State = EntityState.Modified;
+                 db.SaveChanges();
+                 return RedirectToAction("Index");
+             }
+             ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CompanyName", order.CustomerID);
+             ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "LastName", order.EmployeeID);
+             ViewBag.ShipVia = new SelectList(db.Shippers, "ShipperID", "CompanyName", order.ShipVia);
+             return View(order);
+         }
+         */
+      [HttpPost]
+     // [System.Web.Services.WebMethod]
+      //[HttpGet]
+      [ValidateAntiForgeryToken]
+        public ActionResult Edit(Order data, int? id)
+        {
+            //HttpResponseHeader.ContentType = "text/javascript"
+            //Response.ContentType = ;
+           // string status = "";
+           // Exception exp = null;
+      
+            try
+            {
+                AddData(data, id);
+
+            }
+            catch (Exception e)
+            {
+                //status = "false";
+                Console.WriteLine(e);
+                //exp = e;
+            };
+
+
+            //response = HttpResponse("Text only, please.", mimetype = "text/html")
+            return RedirectToAction("Index");
+           
+        }
+
+        private void AddData(Order data, int? id)
+        {
+
+
+            Order order = db.Orders.Where(x => x.OrderID == id).FirstOrDefault() ;
+            if (data != null)
+            {
+                
+                order.ShipCountry = data.ShipCountry;
+                order.OrderDate = data.OrderDate;
+                order.ShipAddress = data.ShipAddress;
+                order.ShipCity = data.ShipCity;
+                order.ShipName = data.ShipName;
+                order.ShipRegion = data.ShipRegion;
+
+
+            }
+            db.SaveChanges();
+        }
+
+
+
+        // GET: Orders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
